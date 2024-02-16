2aab490ff87881b4cdf372d3fd3ee1f105d3fca9
MustafaaMehdi <anfsmus@gmail.com>
2024-02-16 22:25:35 +0100

    :sparkles: feat(users.js): get & add user feature

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,8 +5,40 @@ const connection = require('../lib/connect.js')
 /* GET users listing. */
 router.get('/', function(req, res, ) {
 
-  connection.connect()
-  res.send('respond with a resource');
+  connection.connect((err) => {
+    if (err) console.log('err', err);
+
+    let query = "SELECT * FROM users"
+
+    connection.query(query, (err, data) => {
+      if (err) console.log('err', err);
+
+      console.log('users', data);;
+      res.json(data)
+    })
+  })
+});
+
+router.post('/add', function(req, res, ) {
+
+  let userId = req.body.userId
+  let userName = req.body.userName
+  let userEmail = req.body.userEmail
+  let password = req.body.password
+
+  connection.connect((err) => {
+    if (err) console.log('err', err);
+
+    let query = "INSERT into users (userId, userName, userEmail, password) VALUES (?,?,?,?)"
+    let values = [userId, userName, userEmail,password]
+
+    connection.query(query,values, (err, data) => {
+      if (err) console.log('err', err);
+
+      console.log('users', data);;
+      res.json({data})
+    })
+  })
 });
 
 module.exports = router;
